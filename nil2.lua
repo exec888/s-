e6e6159d0e30a1bc005b6946bb3ff1a15c3d98f3
exec888 <117951720+exec888@users.noreply.github.com>
2022-11-15 20:06:26 +0500

    Update nil2.lua

--- nil2.lua
+++ nil2.lua
@@ -1,5 +1,5 @@
 local UILibrary = {}
-_G.Version = "3G"
+_G.Version = "4A"
 
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Player788/luau1/main/lib.lua"))()
 local Players = game:GetService("Players")
@@ -1564,7 +1564,7 @@ function UILibrary:Window(Table)
 				if config.Save then 
 					local rgb = Sync(0, {Table.Key..".txt", game:GetService("HttpService"):JSONEncode( {R = Table.Default.R, G = Table.Default.G , B = Table.Default.B })})
 					print(rgb)
-					rgb = Color3.fromRGB(rgb.R, rgb.G, rgb.B)
+					rgb = Color3.fromRGB(rgb.R*255, rgb.G*255, rgb.B*255)
 					Colorpicker:Set(rgb)	
 				end
 
