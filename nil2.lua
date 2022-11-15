73b3793942a2cb53e2fdd8b38f2e9889604c2fed
exec888 <117951720+exec888@users.noreply.github.com>
2022-11-15 15:37:19 +0500

    Update nil2.lua

--- nil2.lua
+++ nil2.lua
@@ -1,5 +1,5 @@
 local UILibrary = {}
-_G.Version = "1B"
+_G.Version = "1C"
 
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Player788/luau1/main/lib.lua"))()
 local Players = game:GetService("Players")
